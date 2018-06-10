d2acfb009921313b284b9d6995367fc589b38c16
skittles <julia.heath@me.com>
2018-06-10 16:22:18 -0700

    created second function and added title replacement

--- build1.py
+++ build1.py
@@ -13,60 +13,25 @@ pages = [
     {
         'content': 'content/projects.html',
         'output_file': 'docs/projects.html',
-        'ttile' : 'Projects',
+        'title' : 'Projects',
     },
        {
         'content': 'content/contact.html',
         'output_file': 'docs/contact.html',
-        'ttile' : 'Contact',
+        'title' : 'Contact',
     },
 ]
 
 
 def main():
-    
-
     for page in pages:
-        print(page['content'])
-        contents_of_file = open(page['content']).read()
-        print(contents_of_file)
-        templates = open('templates/base.html').read()
-        docs = templates.replace('{{content}}', contents_of_file)
-        open(page['output_file'], 'w+').write(docs)
-        
-        
-        
-#    content_index = open("content/index.html").read()
-# 
-
-#    home = base + content_index
-
-#    open("docs/index.html", "w+").write(home) 
-
-#    
-#  
-#    content_blog = open("content/blog.html").read()
-
-#    blog = base + content_blog
-
-#    open("docs/blog.html", "w+").write(blog) 
-#    
-#    
-
-#    content_projects = open("content/projects.html").read()
-
-#    projects = base + content_projects
-
-#    open("docs/projects.html", "w+").write(projects) 
-
-
-
-#    content_contact = open("content/contact.html").read()
-
-#    contact = base + content_contact
-
-#    open("docs/contact.html", "w+").write(contact) 
-    
+       contents_of_file = open(page['content']).read()
+       templates = open('templates/base.html').read()
+       title_of_file = (page['title'])
+       def title():
+            docs = templates.replace('{{content}}', contents_of_file).replace('{{title}}', title_of_file)
+            open(page['output_file'], 'w+').write(docs)
+       title()   
 main()
 
 
